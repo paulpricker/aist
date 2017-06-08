e0584eb065238f1c1a6c175357ee4f461e93b66c
paulpricker <paulpricker@gmail.com>
2017-06-09 00:39:49 +0300

    CreateDonor version 0.71
    Привёл в порядок код, удалив лишние строки с комментариями

--- cd.py
+++ cd.py
@@ -40,12 +40,10 @@ class Test_CreateDonor(unittest.TestCase):
 		driver.find_element_by_id('IdentityDocument_IssueDate').send_keys('09032010')
 		driver.find_element_by_id('NextStep').click()
 		#Добавить assert
-<<<<<<< HEAD
-=======
+
 
 	def tearDown(self):
 		self.driver.quit()
->>>>>>> 71066111c62b2b6e84e7b05e405335a76f115b29
 
 	def tearDown(self):
 		self.driver.quit()
