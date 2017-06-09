b3cf175ffbd09c9aa037df90ed0f57a024ac4313
Pavel <paulpricker@gmail.com>
2017-06-09 15:19:22 +0300

    v0.72
    Правильно расставил ожидания.

--- cd.py
+++ cd.py
@@ -20,20 +20,19 @@ class Test_CreateDonor(unittest.TestCase):
 		submit.click()
 		WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".donor-logo")))
 
-	def test_create(self, lastname = 'Мэттерз', firstname = 'Маршал'):
+	def test_create(self):
 		driver = self.driver
 		wait = WebDriverWait(driver, 10)
-		self.lastname = lastname
-		self.firstname = firstname
 		driver.get('http://10.32.200.127/donor')
+		newdonor = driver.find_element_by_xpath(".//*[@id='newdonor']")
+		last = driver.find_element_by_xpath('//*[@id="LastName"]')
+		first = driver.find_element_by_id('FirstName')
 		wait.until(EC.element_to_be_clickable((By.XPATH, ".//*[@id='newdonor']")))
-		driver.find_element_by_xpath(".//*[@id='newdonor']").click()
+		newdonor.click()
 		#Добавить assert
-		#LastName = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='LastName']")))
-		LastName = driver.find_element_by_id('LastName')
-		LastName.send_keys(self.lastname)
-		FirstName = driver.find_element_by_id('FirstName')
-		FirstName.send_keys(self.firstname)
+		wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='LastName']"))) #or element_to_be_clickable
+		last.send_keys('Уколов')
+		first.send_keys('Павел')
 		driver.find_element_by_id('BirthDate').send_keys('11021990')
 		driver.find_element_by_id('Gender').click()
 		driver.find_element_by_id('IdentityDocument_Serie').send_keys('0956980716')
@@ -41,13 +40,8 @@ class Test_CreateDonor(unittest.TestCase):
 		driver.find_element_by_id('NextStep').click()
 		#Добавить assert
 
-
 	def tearDown(self):
 		self.driver.quit()
-
-	def tearDown(self):
-		self.driver.quit()
-
 
 if __name__ == '__main__':
 	unittest.main()
