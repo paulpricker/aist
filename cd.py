c16616420238a3e1772e06225389b03fbce56685
Pavel <paulpricker@gmail.com>
2017-06-14 18:55:31 +0300

    CreateDonor v0.73
    Продолжил работу над второй страницей. Нужно разобраться с длинными xpath'ами и номерами div'ов.

--- cd.py
+++ cd.py
@@ -13,20 +13,23 @@ from selenium.webdriver.support.ui import Select
 class Test_CreateDonor(unittest.TestCase):
 
 	def setUp(self):
+		'''Переход на страницу и логин'''
 		self.driver = webdriver.Firefox()
 		self.driver.get('http://10.32.200.127')
 		self.driver.find_element_by_id('Login').send_keys('admin')
 		self.driver.find_element_by_id('Password').send_keys('77@dm1n')
 		submit = self.driver.find_element_by_class_name('submit')
 		submit.click()
-		WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".donor-logo")))
+		WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".donor-logo")))
+		# добавить assert
 
 	def test_create(self):
+		'''Проверка первой страницы pop-up'а'''
 		driver = self.driver
-		wait = WebDriverWait(driver, 10)
+		wait = WebDriverWait(driver, 20)
 		driver.get('http://10.32.200.127/donor')
+		wait.until(EC.presence_of_element_located((By.XPATH, ".//*[@id='newdonor']"))) #element_to_be_clickable
 		newdonor = driver.find_element_by_xpath(".//*[@id='newdonor']")
-		wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='newdonor']")))
 		newdonor.click()
 		wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[21]/div[2]/form/div/div[2]/div[1]/input"))) #or element_to_be_clickable
 		last = driver.find_element_by_xpath('//*[@id="LastName"]') #//*[@id="LastName"]
@@ -38,14 +41,20 @@ class Test_CreateDonor(unittest.TestCase):
 		driver.find_element_by_id('IdentityDocument_Serie').send_keys('0956980716')
 		driver.find_element_by_id('IdentityDocument_IssueDate').send_keys('09032010')
 		driver.find_element_by_id('NextStep').click()
-		#Добавить assert
-		#http://software-testing.ru/forum/index.php?/topic/28840-problemy-s-select-vypadaiuschij-spisok/
+
+		'''Проверка второй страницы pop-up'а'''
 		wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='regFiasAddress_Region']")))
+		# добавить assert
 		assert driver.find_element_by_xpath('//*[@id="regFiasAddress_Street"]').is_enabled() == False #get_attribute('disabled') == True
 		driver.find_element_by_id('regFiasAddress_Region').send_keys('Мос')
 		wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[74]/div/div[2]/ul/li[1]")))
 		select_region = driver.find_element_by_xpath('/html/body/div[74]/div/div[2]/ul/li[1]')
 		select_region.click()
+		driver.find_element_by_xpath('//*[@id="regFiasAddress_Street"]').send_keys('Строит')
+		wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[80]/div/div[2]/ul/li[2]")))
+		select_street = driver.find_element_by_xpath('/html/body/div[120]/div/div[2]/ul/li[2]')
+		select_street.click()
+
 		
 	def tearDown(self):
 		self.driver.quit()
