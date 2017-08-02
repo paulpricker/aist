e4908da1c82e49ba639d53fa4dba1220eae468a4
Pavel <paulpricker@gmail.com>
2017-08-02 13:41:48 +0300

    CreateDonor v0.8
    Успешное создание донора, ошибок не возникает, Тикет #1 закрыт.

--- cd.py
+++ cd.py
@@ -34,26 +34,31 @@ class Test_CreateDonor(unittest.TestCase):
 		wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[21]/div[2]/form/div/div[2]/div[1]/input"))) #or element_to_be_clickable
 		last = driver.find_element_by_xpath('//*[@id="LastName"]') #//*[@id="LastName"]
 		first = driver.find_element_by_id('FirstName')
-		last.send_keys('Мэттерз')
-		first.send_keys('Автотестовый')
+		last.send_keys('Машинный')
+		first.send_keys('Яша')
 		driver.find_element_by_id('BirthDate').send_keys('11021990')
 		driver.find_element_by_id('Gender').click()
 		driver.find_element_by_id('IdentityDocument_Serie').send_keys('0956980716')
 		driver.find_element_by_id('IdentityDocument_IssueDate').send_keys('09032010')
 		driver.find_element_by_id('NextStep').click()
-
 		'''Проверка второй страницы pop-up'а'''
 		wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='regFiasAddress_Region']")))
 		# добавить assert
 		assert driver.find_element_by_xpath('//*[@id="regFiasAddress_Street"]').is_enabled() == False #get_attribute('disabled') == True
+		save_button = driver.find_element_by_xpath('//*[@id="save-newdonor"]')
+		#assert save_button.click()
 		driver.find_element_by_id('regFiasAddress_Region').send_keys('Мос')
-		wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[74]/div/div[2]/ul/li[1]")))
-		select_region = driver.find_element_by_xpath('/html/body/div[74]/div/div[2]/ul/li[1]')
+		wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="regFiasAddress_Region_listbox"]/li[1]')))
+		select_region = driver.find_element_by_xpath('//*[@id="regFiasAddress_Region_listbox"]/li[1]')
 		select_region.click()
+		time.sleep(2)
+		#assert save_button.click()
 		driver.find_element_by_xpath('//*[@id="regFiasAddress_Street"]').send_keys('Строит')
-		wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[80]/div/div[2]/ul/li[2]")))
-		select_street = driver.find_element_by_xpath('/html/body/div[120]/div/div[2]/ul/li[2]')
-		select_street.click()
+		wait.until(EC.text_to_be_present_in_element((By.XPATH, '//*[@id="regFiasAddress_Street_listbox"]/li[2]'), 'Строителей ул'))
+		#assert save_button.click()
+		driver.find_element_by_xpath('//*[@id="regFiasAddress_Street_listbox"]/li[2]').click()
+		driver.find_element_by_xpath('//*[@id="regFiasAddress_House"]').send_keys('13')
+		save_button.click()
 
 		
 	def tearDown(self):
@@ -63,15 +68,16 @@ if __name__ == '__main__':
 	unittest.main()
 '''
 
-ЗАПРОС №1 - Дописать класс.
+TICKET #1 - Дописать класс.
 Класс должен выполнять без ошибок основную функциональность: заходить на сайт, логиниться, создавать донора.
 Только после этого можно переходить к работе над Запросом №2.
+RESOLUTION: Done V
 
-ЗАПРОС №2 - Добавить проверки.
-1)выделить контрольные точки, в которых тестировщик бы анализировал результат выполнения тест-кейса;
+TICKET №2 - Добавить проверки (Is blocked by TICKET #1).
+1) выделить контрольные точки, в которых тестировщик бы анализировал результат выполнения тест-кейса;
 2) добавить в эти контрольные точки assertions.
 
-ЗАПРОС №3 - Реализовать приём тестовых данных из отдельного модуля классом Test_CreateDonor.
+TICKET №3 - Реализовать приём тестовых данных из отдельного модуля классом Test_CreateDonor (Is blocked by TICKET #2).
 1) необходимо продумать логику реализации приёма этим классом тестовых данных - как он будет принимать данные для теста;
 2) необходимо продумать, каким будет файл с данными для теста (возможно, в формате json, либо тот же .py), каким образом он будет отдавать данные классу;
 3) придумать тестовые данные и запустить тест.
